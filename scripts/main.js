31259ec53439b40d48ec649a5d554cc748eeed5d
Leon Peck <leonpeck234@gmail.com>
2014-10-13 15:55:09 -0500

    Simplified the JS a bit and tweaked CSS more

--- scripts/main.js
+++ scripts/main.js
@@ -4,26 +4,33 @@ document.addEventListener('DOMContentLoaded', function(){
   var lightbox = document.querySelector('.lightbox');
   var loginModal = document.querySelector('.login-modal');
 
-  signIn.addEventListener('click', function(){
-    show(loginModal);
-  });
+var loginModalToggleAreas = document.querySelectorAll(
+  '.lightbox, .login, input[value="Cancel"]'
+);
 
+function toggle(element) {
+  if (element.classList) {
+    element.classList.toggle('hidden')
+} else {
+    var classes = element.className.split(' ');
+    var classIndex;
+    for (var i = 0; i < classes.length; i++) {} 
+      if (classes[i] === 'hidden')
+        classIndex = i;
+    }
 
-  cancel.addEventListener('click', function(){
-    hide(loginModal);
-  });
+    if (classIndex)
+      classes.splice(classIndex, 1);
+    else
+      classes.push('hidden');
 
-    
-  lightbox.addEventListener('click', function(){
-    hide(loginModal);
-  });
+  element.className = classes.join(' ');
+  }
+}
+
+for (var i = 0; i < loginModalToggleAreas.length; i++){
+   loginModalToggleAreas[i].addEventListener('click', function() {
+      toggle(loginModal);
+    });
+  }
 });
-
-function hide(element) {
-  element.style.display = "none";
-}
-
-function show(element) {
-element.style.display = "block";
-}
-
